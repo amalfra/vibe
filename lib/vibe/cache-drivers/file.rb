cf961b2941a375a6ac7c6863695a61093d4471b0
Amal Francis <amalfra@gmail.com>
2014-10-30 21:38:53 +0530

    Added automatic cache purging after a day

--- lib/vibe/cache-drivers/file.rb
+++ lib/vibe/cache-drivers/file.rb
@@ -2,7 +2,7 @@ module Vibe
   class File
 
     def initialize(cache_dir = '')
-      @cache_dir = '/tmp/vibe_gem/' if cache_dir == ''
+      @cache_dir = Dir.tmpdir()+'/vibe_gem/' if cache_dir == ''
 
       if !Dir.exists?(@cache_dir)
           Dir.mkdir(@cache_dir)
@@ -11,6 +11,13 @@ module Vibe
 
     def get(key = '')
       if key != '' && ::File.exists?(@cache_dir+key)
+        # Is the File older than a day?
+        if file_age(@cache_dir+key) > 1
+          # Delete and return a cache miss
+          File.delete(@cache_dir+key)
+          return false
+        end
+
         puts "Reading from cache with key: #{key}" if ENV['DEBUG']
         data = ::File.read(@cache_dir+key)
         return (data.length > 0) ? data : false
@@ -26,6 +33,10 @@ module Vibe
       end
 
       return false
+    end
+
+    def file_age(name)
+      (Time.now - ::File.stat(name).mtime)/(24*3600)
     end
 
   end # File
